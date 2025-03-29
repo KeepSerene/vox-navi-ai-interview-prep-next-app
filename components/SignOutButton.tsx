71e497c64dd1f58f925b87320412760a6643e21e
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-03-29 20:42:12 +0530

    minor tweak

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -24,8 +24,6 @@ function SignOutButton({ userId }: { userId: string }) {
         title="Sign out"
         className="btn-secondary"
       >
-        <span className="max-md:hidden">Sign out</span>
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -40,6 +38,8 @@ function SignOutButton({ userId }: { userId: string }) {
             d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM4 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 10.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
           />
         </svg>
+
+        <span className="max-md:hidden">Sign out</span>
       </Button>
 
       <SignOutModal isOpen={isModalOpen} userId={userId} onClose={closeModal} />
